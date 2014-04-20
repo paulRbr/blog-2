8a871743d663ead438eaedceb1e34c47a9f35987
Tristram Gräbener <tristramg@gmail.com>
2014-04-20 17:59:59 +0200

    Updated personal data

--- pelicanconf.py
+++ pelicanconf.py
@@ -11,8 +11,8 @@ DEFAULT_LANG = 'fr'
 
 # Blogroll
 LINKS =  (('Pelican (moteur de ce blog)', 'http://docs.notmyidea.org/alexis/pelican/'),
-          ('Canal TP (mon employeur)', 'http://www.canaltp.fr'),
-          (u"Blog de mon boulot où j\'écris parfois", 'http://labs.canaltp.fr'),)
+          ('Capitaine Train (mon employeur)', 'http://www.capitainetrain.com'),
+          (u"Blog de mon (ancien) boulot où j\'écris (j’écrivais) parfois", 'http://labs.canaltp.fr'),)
 
 # Social widget
 SOCIAL = (('GitHub', 'https://github.com/Tristramg'),
